6a58d480a4da7e7f2e3ca3239c10cf163e1f1bed
xynqww <princeclainedump85@gmail.com>
2024-09-07 03:34:54 +0800

    Update api.py

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ import time
 
 app = Flask(__name__)
 
-key_regex = r'let content = "([^"]+)"'
+key_regex = r'let content = \("([^"]+)"\);'
 
 def fetch(url, headers):
     try:
@@ -22,18 +22,17 @@ def bypass_link(url):
             raise Exception("Invalid HWID in URL")
 
         start_time = time.time()
-
         endpoints = [
             {
                 "url": f"https://flux.li/android/external/start.php?HWID={hwid}",
                 "referer": ""
             },
             {
-                "url": f"https://flux.li/android/external/check1.php?hash={hwid}",
+                "url": "https://flux.li/android/external/check1.php?hash={hash}",
                 "referer": "https://linkvertise.com"
             },
             {
-                "url": f"https://flux.li/android/external/main.php?hash={hwid}",
+                "url": "https://flux.li/android/external/main.php?hash={hash}",
                 "referer": "https://linkvertise.com"
             }
         ]
@@ -50,10 +49,6 @@ def bypass_link(url):
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x66) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
             }
             response_text = fetch(url, headers)
-
-            print(f"DEBUG: Response from {url}:")
-            print(response_text)
-
             if endpoint == endpoints[-1]:
                 match = re.search(key_regex, response_text)
                 if match:
@@ -61,7 +56,7 @@ def bypass_link(url):
                     time_taken = end_time - start_time
                     return match.group(1), time_taken
                 else:
-                    raise Exception("Failed to find content key in the response")
+                    raise Exception("Failed to find content key")
     except Exception as e:
         raise Exception(f"Failed to bypass link. Error: {e}")
 
@@ -72,14 +67,14 @@ def home():
 @app.route("/api/fluxus")
 def bypass():
     url = request.args.get("url")
-    if url and url.startswith("https://flux.li/android/external/start.php?HWID="):
+    if url.startswith("https://flux.li/android/external/start.php?HWID="):
         try:
             content, time_taken = bypass_link(url)
             return jsonify({"key": content, "time_taken": time_taken, "credit": "FeliciaXxx"})
         except Exception as e:
             return jsonify({"error": str(e)}), 500
     else:
-        return jsonify({"message": "Please Enter a valid Fluxus Link!"})
+        return jsonify({"message": "Please Enter Fluxus Link!"})
 
 if __name__ == "__main__":
     app.run()
